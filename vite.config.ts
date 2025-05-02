60208ef9764670de38e805ba0effb27476f55052
legutierrezc1987 <114323577+legutierrezc1987@users.noreply.github.com>
2025-05-02 00:22:36 -0600

    Configurar dominio personalizado (pitea2.us)

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/pitea2/',  // Ajustado al nombre del repositorio para GitHub Pages
+  base: '/',  // Cambiado para usar dominio personalizado
 })
