ce16013a134a7b34b5e735251e797f4aadccf4ba
legutierrezc1987 <114323577+legutierrezc1987@users.noreply.github.com>
2025-05-01 23:57:06 -0600

    Fix base URL for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/Pitea2/', // Nombre del repositorio
+  base: '/',          // o simplemente elimina la línea 'base'
+  // …
 })
